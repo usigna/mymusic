48723d72e3a67417f8533fdf652afe68bbd8a772
usigna <bukazaokna@gmail.com>
2021-12-15 22:52:56 +0100

    Add function activeInput

--- js/app.js
+++ js/app.js
@@ -14,33 +14,17 @@ function showBtnAnimation() {
       ripples.remove();
     }, 1000);
   });
-
-  // buttons.forEach(btn => {
-  //   btn.addEventListener('mouseover', function(e) {
-  //     let x = e.clientX - e.target.offsetLeft;
-  //     let y = e.clientY - e.target.offsetTop;
-    
-  //     let ripples = document.createElement('span');
-  //     ripples.style.left = x + 'px';
-  //     ripples.style.top = y + 'px';
-  //     this.appendChild(ripples);
-      
-  //     setTimeout(() => {
-  //       ripples.remove();
-  //     }, 1000);
-  //   });
-  // })
 }
 
 function showImage() {
   const inputFile = document.querySelector('.file');
   const imagePreview = document.querySelector('.image-preview__image');
-  // const name = document.getElementById('name').value;
-  // const surname = document.getElementById('surname').value;
+  const name = document.getElementById('name').value;
+  const surname = document.getElementById('surname').value;
 
   inputFile.addEventListener('change', function() {
     const file = this.files[0];
-    // const fullName = name + " " + surname;
+    const fullName = name + " " + surname;
 
     if(file) {
       const reader = new FileReader();
@@ -48,46 +32,38 @@ function showImage() {
 
       reader.addEventListener('load', function() {
         imagePreview.setAttribute('src', this.result);
-        // imagePreview.setAttribute('alt', fullName);
+        imagePreview.setAttribute('alt', fullName);
       });
 
       reader.readAsDataURL(file);
     } else {
       imagePreview.style.display = null;
       imagePreview.setAttribute('src', '');
-      // imagePreview.setAttribute('alt', '');
+      imagePreview.setAttribute('alt', '');
     }
   });
 }
 
-
-
-
-
-function show(name) {
-  document.querySelector(name).classList.add('show');
-  document.querySelector(name).classList.remove('hide');
-}
-
-function hide(name) {
-  document.querySelector(name).classList.add('hide');
-  document.querySelector(name).classList.remove('show');
-}
-
-function showBox() {
+function activeInput() {
   const radioItems = document.querySelectorAll('.radio input');
+  const pesel = document.querySelector('.pesel');
+  const nip = document.querySelector('.nip');
 
-  for (let i = 0; i < radioItems.length; i++) {
-    radioItems[i].addEventListener('change', function() {
-      if (radioItems[i].value == 'person') {
-        show('.idnumber--pesel');
-        hide('.idnumber--nip');
+  radioItems.forEach(item => {
+    item.addEventListener('change', function() {
+      if (item.value == 'person') {
+        setEnabled(pesel, nip);
       } else {
-        show('.idnumber--nip');
-        hide('.idnumber--pesel');
+        setEnabled(nip, pesel);
       }
     });
-  }
+  })
+}
+
+function setEnabled(x, y) {
+  x.setAttribute('enabled', '');
+  x.removeAttribute('disabled', '');
+  y.setAttribute('disabled', '');
 }
 
 // function isValidNip(nip) {
@@ -110,7 +86,7 @@ function showBox() {
 const init = function () {
   showBtnAnimation();
   showImage();
-  // showBox();
+  activeInput();
   // console.log(isValidNip('95-62-360-263'));
 };
 
